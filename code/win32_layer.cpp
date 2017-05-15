97f9522c3738b36924281cdbb7b14b8a472b68fc
BARRET GAYLOR <BARGAYLO@uat.edu>
2017-05-14 22:53:44 -0700

    work on day 238

--- code/win32_layer.cpp
+++ code/win32_layer.cpp
@@ -5,7 +5,21 @@
    $Creator: Barret Gaylor $
    $Notice: (C) Copyright 2017 by Barret Gaylor, Inc. All Rights Reserved. $
    ======================================================================== */
+
+#define PLATFORM_WINDOWS  1
+#define PLATFORM_MAC      2
+#define PLATFORM_UNIX     3
+
+#if defined(_WIN32)
+    #define PLATFORM PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+    #define PLATFORM PLATFORM_MAC
+#else
+    #define PLATFORM PLATFORM_UNIX
+#endif
+
 #include <windows.h>
+
 #include <stdio.h>
 #include "timing.h"
 #include <math.h>
