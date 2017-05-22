4ec60a66e7b22db6230049eecfd3154712e5a84f
BARRET GAYLOR <BARGAYLO@uat.edu>
2017-05-22 00:28:58 -0700

    day 246

--- code/opengl.cpp
+++ code/opengl.cpp
@@ -35,6 +35,8 @@
 #define WGL_FULL_ACCELERATION_ARB                 0x2027
 #define WGL_TYPE_RGBA_ARB                         0x202B
 
+#define WGL_FRAMEBUFFER_SRBG_CAPABLE_ARB          0x20A9
+
 struct opengl_info
 {
     bool ModernContext;
@@ -136,73 +138,150 @@ GLuint CompileShaders(render_fuctions *GLFuctions)
 static unsigned int TextureHandleCount = 0;
 static GLuint OpenGLDefaultTextureFormat;
 
-void
-Win32InitOpenGL(HWND Window, render_fuctions *Functions, GLuint *Program)
+static wgl_create_context_attribts_arb *wglCreateContextAttribsARB;
+static wgl_choose_pixel_format_arb *wglChoosePixelFormatARB;
+static wgl_swap_interval_ext *wglSwapInterval;
+
+static void 
+Win32SetPixelFormat(HDC WindowDC)
 {
+    int SuggestedPixelFormatIndex = 0;
+    GLuint ExtendedPick = 0; 
+    if(wglChoosePixelFormatARB)
+    {
+        int IntAttribList[] = 
+        {
+            WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+            WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+            WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+            WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+            WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+            WGL_FRAMEBUFFER_SRBG_CAPABLE_ARB, GL_TRUE, 
+            0
+        };
+        
+        float FloatAttribList[] = {0};
+        wglChoosePixelFormatARB(WindowDC, IntAttribList, FloatAttribList, 
+                                1, &SuggestedPixelFormatIndex, &ExtendedPick);
+        
+    }
     
-    HDC WindowDC = GetDC(Window);
+    if(!ExtendedPick)
+    {
+        PIXELFORMATDESCRIPTOR DesiredPixelFormat = {};
+        DesiredPixelFormat.nSize = sizeof(DesiredPixelFormat);
+        DesiredPixelFormat.nVersion = 1;
+        DesiredPixelFormat.iPixelType = PFD_TYPE_RGBA;
+        
+        DesiredPixelFormat.dwFlags = PFD_SUPPORT_OPENGL|PFD_DRAW_TO_WINDOW|PFD_DOUBLEBUFFER;
+        
+        DesiredPixelFormat.cColorBits = 32;
+        DesiredPixelFormat.cAlphaBits = 8;
+        DesiredPixelFormat.iLayerType = PFD_MAIN_PLANE;
+        
+        SuggestedPixelFormatIndex = ChoosePixelFormat(WindowDC, &DesiredPixelFormat);
+        
+    }
     
-    PIXELFORMATDESCRIPTOR DesiredPixelFormat = {};
-    DesiredPixelFormat.nSize = sizeof(DesiredPixelFormat);
-    DesiredPixelFormat.nVersion = 1;
-    DesiredPixelFormat.iPixelType = PFD_TYPE_RGBA;
-    DesiredPixelFormat.dwFlags = PFD_SUPPORT_OPENGL|PFD_DRAW_TO_WINDOW|PFD_DOUBLEBUFFER;
-    DesiredPixelFormat.cColorBits = 32;
-    DesiredPixelFormat.cAlphaBits = 8;
-    DesiredPixelFormat.iLayerType = PFD_MAIN_PLANE;
-    
-    int SuggestedPixelFormatIndex = ChoosePixelFormat(WindowDC, &DesiredPixelFormat);
     PIXELFORMATDESCRIPTOR SuggestedPixelFormat;
     DescribePixelFormat(WindowDC, SuggestedPixelFormatIndex,
                         sizeof(SuggestedPixelFormat), &SuggestedPixelFormat);
-    SetPixelFormat(WindowDC, SuggestedPixelFormatIndex, &SuggestedPixelFormat);       
-    Functions->glClearBufferfv = (PFNGLCLEARBUFFERFVPROC)GetAnyGLFuncAddress("glClearBufferfv");
-    Functions->glCreateShader = (PFNGLCREATESHADERPROC)GetAnyGLFuncAddress("glCreateShader");
-    Functions->glShaderSource = (PFNGLSHADERSOURCEPROC)GetAnyGLFuncAddress("glShaderSource");
-    Functions->glCompileShader = (PFNGLCOMPILESHADERPROC)GetAnyGLFuncAddress("glCompileShader");    
-    Functions->glCreateProgram = (PFNGLCREATEPROGRAMPROC)GetAnyGLFuncAddress("glCreateProgram");
-    Functions->glAttachShader = (PFNGLATTACHSHADERPROC)GetAnyGLFuncAddress("glAttachShader"); 
-    Functions->glLinkProgram = (PFNGLLINKPROGRAMPROC)GetAnyGLFuncAddress("glLinkProgram");
-    Functions->glDeleteShader = (PFNGLDELETESHADERPROC)GetAnyGLFuncAddress("glDeleteShader");         
-    Functions->glCreateVertexArrays = (PFNGLCREATEVERTEXARRAYSPROC)GetAnyGLFuncAddress("glCreateVertexArrays");
-    Functions->glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC)GetAnyGLFuncAddress("glBindVertexArray");
-    Functions->glDeleteVertexArrays = (PFNGLDELETEVERTEXARRAYSPROC) GetAnyGLFuncAddress("glDeleteVertexArrays");
-    Functions->glDeleteProgram = (PFNGLDELETEPROGRAMPROC)GetAnyGLFuncAddress("glDeleteProgram");
-    Functions->glUseProgram = (PFNGLUSEPROGRAMPROC)GetAnyGLFuncAddress("glUseProgram");      
-    Functions->glDrawArrays = (PFNGLDRAWARRAYSEXTPROC)GetAnyGLFuncAddress("glDrawArrays");
+    SetPixelFormat(WindowDC, SuggestedPixelFormatIndex, &SuggestedPixelFormat);
     
-    HGLRC OpenGLRC = wglCreateContext(WindowDC);
+}
+
+static void 
+Win32LoadWGLExtensions()
+{
+    WNDCLASSA WindowClass = {};
+    
+    WindowClass.lpfnWndProc = DefWindowProc;
+    WindowClass.hInstance = GetModuleHandle(0);
+    WindowClass.lpszClassName = "WGLLoader";
+    
+    if(RegisterClassA(&WindowClass))
+    {
+        HWND Window = CreateWindowExA(
+            0,
+            WindowClass.lpszClassName,
+            "OpenGL",
+            0, 
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            WindowClass.hInstance,
+            0);
+        
+        HDC WindowDC = GetDC(Window);
+        Win32SetPixelFormat(WindowDC);
+        HGLRC OpenGLRC = wglCreateContext(WindowDC);
+        if(wglMakeCurrent(WindowDC, OpenGLRC))
+        {
+            wglCreateContextAttribsARB = 
+                (wgl_create_context_attribts_arb *)wglGetProcAddress("wglCreateContextAttribsARB");
+            wglChoosePixelFormatARB = (wgl_choose_pixel_format_arb *)wglGetProcAddress("wglChoosePixelFormatARB");
+            wglSwapInterval = (wgl_swap_interval_ext *)wglGetProcAddress("wglSwapIntervalEXT");
+            
+            wglMakeCurrent(0, 0);
+        }
+        
+        wglDeleteContext(OpenGLRC);
+        ReleaseDC(Window, WindowDC);
+        DestroyWindow(Window);
+    }
+}
+
+int Win32OpenGLAttribs[] =
+{
+    WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+    WGL_CONTEXT_MINOR_VERSION_ARB, 0,
+    WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_DEBUG_BIT_ARB,
+    WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB,
+    0,
+};
+
+HGLRC
+Win32InitOpenGL(HWND Window, render_fuctions *Functions, GLuint *Program)
+{
+    Win32LoadWGLExtensions();
+    HDC WindowDC = GetDC(Window);
+    
+    bool ModernContext = true;
+    HGLRC OpenGLRC = 0;
+    if(wglCreateContextAttribsARB)
+    {
+        Win32SetPixelFormat(WindowDC);
+        OpenGLRC = wglCreateContextAttribsARB(WindowDC, 0, Win32OpenGLAttribs);
+    }
+    
+    if(!OpenGLRC)
+    {
+        ModernContext = false;
+        OpenGLRC = wglCreateContext(WindowDC);
+    }
+    
     if(wglMakeCurrent(WindowDC, OpenGLRC))
     {
-        bool ModernContext = false; 
-        wgl_create_context_attribts_arb *wglCreateContextAttribsARB = (wgl_create_context_attribts_arb *)wglGetProcAddress("wglCreateContextAttribsARB");
-        if(wglCreateContextAttribsARB)
-        {
-            // NOTE(barret): this is a modern version of OpenGL
-            int Attribs[] =
-            {
-                WGL_CONTEXT_MAJOR_VERSION_ARB,   3,
-                WGL_CONTEXT_MINOR_VERSION_ARB, 0,
-                WGL_CONTEXT_FLAGS_ARB, WGL_CONTEXT_DEBUG_BIT_ARB,//|WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
-                WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB,
-                0
-            };
-            HGLRC ShareContext = 0;
-            HGLRC ModernGLRC = wglCreateContextAttribsARB(WindowDC, ShareContext, Attribs);
-            if(ModernGLRC)
-            {
-                if(wglMakeCurrent(WindowDC, ModernGLRC))
-                {
-                    ModernContext = true; 
-                    wglDeleteContext(OpenGLRC);
-                    OpenGLRC = ModernGLRC;
-                }
-            }
-        }
-        else
-        {
-            // NOTE(barret): this is an antiquated version of OpenGL
-        }
+        
+        
+        Functions->glClearBufferfv = (PFNGLCLEARBUFFERFVPROC)GetAnyGLFuncAddress("glClearBufferfv");
+        Functions->glCreateShader = (PFNGLCREATESHADERPROC)GetAnyGLFuncAddress("glCreateShader");
+        Functions->glShaderSource = (PFNGLSHADERSOURCEPROC)GetAnyGLFuncAddress("glShaderSource");
+        Functions->glCompileShader = (PFNGLCOMPILESHADERPROC)GetAnyGLFuncAddress("glCompileShader");    
+        Functions->glCreateProgram = (PFNGLCREATEPROGRAMPROC)GetAnyGLFuncAddress("glCreateProgram");
+        Functions->glAttachShader = (PFNGLATTACHSHADERPROC)GetAnyGLFuncAddress("glAttachShader"); 
+        Functions->glLinkProgram = (PFNGLLINKPROGRAMPROC)GetAnyGLFuncAddress("glLinkProgram");
+        Functions->glDeleteShader = (PFNGLDELETESHADERPROC)GetAnyGLFuncAddress("glDeleteShader");         
+        Functions->glCreateVertexArrays = (PFNGLCREATEVERTEXARRAYSPROC)GetAnyGLFuncAddress("glCreateVertexArrays");
+        Functions->glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC)GetAnyGLFuncAddress("glBindVertexArray");
+        Functions->glDeleteVertexArrays = (PFNGLDELETEVERTEXARRAYSPROC) GetAnyGLFuncAddress("glDeleteVertexArrays");
+        Functions->glDeleteProgram = (PFNGLDELETEPROGRAMPROC)GetAnyGLFuncAddress("glDeleteProgram");
+        Functions->glUseProgram = (PFNGLUSEPROGRAMPROC)GetAnyGLFuncAddress("glUseProgram");      
+        Functions->glDrawArrays = (PFNGLDRAWARRAYSEXTPROC)GetAnyGLFuncAddress("glDrawArrays");
+        
         opengl_info Extensions = OpenGLGetInfo(ModernContext);
         
         
@@ -216,38 +295,14 @@ Win32InitOpenGL(HWND Window, render_fuctions *Functions, GLuint *Program)
         {
             glEnable(GL_FRAMEBUFFER_SRGB);
         }
-        
-        // NOTE(barret): YOU MUST CREATE A CONTEXT FIRST BEFORE GETTING WGL FUNCTIONS
-        wgl_swap_interval_ext *wglSwapInterval = (wgl_swap_interval_ext *)wglGetProcAddress("wglSwapIntervalEXT");
-        
-        if(wglSwapInterval)
-            wglSwapInterval(1);
-        
-        /*
-        wgl_choose_pixel_format_arb *wglChoosePixelFormatARB = (wgl_choose_pixel_format_arb *)wglGetProcAddress("wglChoosePixelFormatARB");
-        
-        int IntAttribList[] = 
-        {
-            WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
-            WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
-            WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
-            WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
-            WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
-            0
-        };
-        
-        float FloatAttribList[] = {0};
-        GLuint ExtendedPick = 0; 
-        wglChoosePixelFormatARB(WindowDC, IntAttribList, FloatAttribList, 
-                                1, &SuggestedPixelFormatIndex, &ExtendedPick);
-    */
     }
-    else
-    {
-        Assert(false);
-    }
+    // NOTE(barret): YOU MUST CREATE A CONTEXT FIRST BEFORE GETTING WGL FUNCTIONS
+    
+    if(wglSwapInterval)
+        wglSwapInterval(1);
     
     ReleaseDC(Window, WindowDC);
+    return(OpenGLRC);
     /* 
        GLuint VertexArrayObject = 0;
        
